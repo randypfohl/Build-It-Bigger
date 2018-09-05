906b3dbd5a6f62a8b7bc909ec83e1df09abad418
Education <randypfohl@gmail.com>
2018-09-04 22:06:16 -0700

    builds the correct fragment per flavor

--- app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -1,4 +1,4 @@
-package com.udacity.gradle.builditbigger.free;
+package com.udacity.gradle.builditbigger;
 
 
 import android.os.Bundle;
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.udacity.gradle.builditbigger.R;
 
 
 /**
