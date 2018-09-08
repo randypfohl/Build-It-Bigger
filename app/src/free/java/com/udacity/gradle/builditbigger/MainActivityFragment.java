17fed3c3f7c0148353936be797cebbacf9baa657
Education <randypfohl@gmail.com>
2018-09-08 09:32:55 -0700

    test ads up and running

--- app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -1,31 +1,108 @@
 package com.udacity.gradle.builditbigger;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-import com.udacity.gradle.builditbigger.R;
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.doubleclick.PublisherAdRequest;
+import com.google.android.gms.ads.doubleclick.PublisherInterstitialAd;
+import com.pfohl.bakingapp.joke.MainActivity;
+
+import java.util.Objects;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MainActivityFragment extends Fragment {
+public class MainActivityFragment extends Fragment implements IJokeCallback{
 
+    private PublisherInterstitialAd interstitialAd = null;
 
     public MainActivityFragment() {
         // Required empty public constructor
     }
 
-
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main_activity, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_main_activity, container, false);
+        Button button =view.findViewById(R.id.joke_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (interstitialAd.isLoaded()) {
+                    interstitialAd.show();
+                } else {
+                    tellJoke();
+                }
+            }
+        });
+
+        AdView mAdView = view.findViewById(R.id.adView);
+
+        interstitialAd = new PublisherInterstitialAd(Objects.requireNonNull(getContext()));
+        interstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
+
+        interstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdClosed() {
+                super.onAdClosed();
+                tellJoke();
+
+                requestNewInterstitial();
+            }
+
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+                super.onAdFailedToLoad(errorCode);
+                requestNewInterstitial();
+
+            }
+
+            @Override
+            public void onAdLoaded() {
+                super.onAdLoaded();
+            }
+        });
+
+        requestNewInterstitial();
+
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+        mAdView.loadAd(adRequest);
+
+        interstitialAd.show();
+
+        return view;
     }
 
+    public void tellJoke(){
+        new JokeAsyncTask().execute(this);
+    }
+
+    @Override
+    public void jokeLoaded(String string) {
+        Intent intent = new Intent(getActivity(), MainActivity.class);
+        intent.putExtra(MainActivity.JOKE_KEY, string);
+        startActivity(intent);
+    }
+
+    private void requestNewInterstitial() {
+        PublisherAdRequest adRequest = new PublisherAdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+
+        interstitialAd.loadAd(adRequest);
+    }
 }
--- app/src/paid/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/paid/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -1,19 +1,22 @@
 package com.udacity.gradle.builditbigger;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.pfohl.bakingapp.joke.MainActivity;
 import com.udacity.gradle.builditbigger.R;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MainActivityFragment extends Fragment {
+public class MainActivityFragment extends Fragment implements IJokeCallback{
 
 
     public MainActivityFragment() {
@@ -22,10 +25,27 @@ public class MainActivityFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main_activity, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+       View view = inflater.inflate(R.layout.fragment_main_activity, container, false);
+       Button button =view.findViewById(R.id.joke_button);
+       button.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               tellJoke();
+           }
+       });
+       return view;
     }
 
+    public void tellJoke(){
+        new JokeAsyncTask().execute(this);
+    }
+
+    @Override
+    public void jokeLoaded(String string) {
+        Intent intent = new Intent(getActivity(), MainActivity.class);
+        intent.putExtra(MainActivity.JOKE_KEY, string);
+        startActivity(intent);
+    }
 }
