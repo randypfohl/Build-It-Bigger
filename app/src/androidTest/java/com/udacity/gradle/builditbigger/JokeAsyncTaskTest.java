e39bc27c6f9a882c8a906ca86f9dbd6de211070f
Education <randypfohl@gmail.com>
2018-09-08 10:09:26 -0700

    added a single test because fml

---
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/JokeAsyncTaskTest.java
@@ -0,0 +1,48 @@
+package com.udacity.gradle.builditbigger;
+
+import android.support.test.espresso.ViewAction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.util.Log;
+import android.view.View;
+
+import org.hamcrest.Matcher;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.*;
+
+@RunWith(AndroidJUnit4.class)
+public class JokeAsyncTaskTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void asyncJokeTaskTest() {
+
+        onView(withId(R.id.joke_button)).perform(click());
+
+        onView(withId(R.id.joke_tv)).check(matches(not(withText(""))));
+    }
+
+}
